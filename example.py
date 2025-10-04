23bdd861483777bbb089d9975da592fb207a0d7f
Andrey Sarmento <andreysarmento10@gmail.com>
2025-10-04 16:29:31 -0300

    Modelo mais robusto atualizado.

--- example.py
+++ example.py
@@ -2,11 +2,11 @@
 from Python.Predict import avaliar_prontuario
 
 # Load a sample clinical note from the folder "Dados"
-with open("Dados/Teste1.txt", encoding="utf-8") as f:
+with open("Dados/Teste3.txt", encoding="utf-8") as f:
     pront_teste = f.read()
 
 # First characters of the medical record
 pront_teste[:300]
 
 # Use the main function
-saida = avaliar_prontuario(pront_teste)
+saida = avaliar_prontuario(pront_teste, delta = 30)
