aacc6197904003c6429faf90f7cde39a4d0b2d1a
Andrey Sarmento <andreysarmento10@gmail.com>
2025-10-13 13:02:49 -0300

    Modelo atualizado.

--- Python/Predict.py
+++ Python/Predict.py
@@ -1,16 +1,21 @@
-# --------------------------------------------------
+# --------------------------------------------------------------------------------------------
 # Bibliotecas
-# --------------------------------------------------
-import torch
-import torch.nn.functional as F
-import pandas as pd
-import re, unicodedata
+# --------------------------------------------------------------------------------------------
 
-# --------------------------------------------------
-# Carregar vocabulário
-# --------------------------------------------------
+import re
+import torch
+import unicodedata
+import pandas as pd
+import torch.nn.functional as F
+
+
+# --------------------------------------------------------------------------------------------
+# Vocabulário e funções de pré-processamento
+# --------------------------------------------------------------------------------------------
+
 with open("Dados/voc.txt", encoding="utf-8") as f:
-    VOC = f.read().splitlines()
+    VOC = f.read().splitlines() # lista de caracteres
+
 
 def Encoder(text, vocabulary=VOC):
     vocab_index = {char: idx for idx, char in enumerate(vocabulary)}
@@ -25,24 +30,30 @@ def fix_len(seq, target_len, pad=0):
 
 def normalizar_prontuario(x, unicode=True):
     if unicode:
-        x = x.lower()
-        x = unicodedata.normalize('NFKD', x).encode('ASCII', 'ignore').decode('utf-8')
-    x = re.sub(r'(\r\n|\r|\n|\\n)+', '. ', x)
-    x = re.sub(r'[ \t]+', ' ', x)
-    x = re.sub(r'\.\s*\.+', '.', x)
-    return x.strip()
+        x = x.lower()  # converte para minúsculas
+        x = unicodedata.normalize('NFKD', x)  # separa acentuação
+        x = x.encode("ASCII", "ignore").decode("ASCII")  # remove acentos e não ASCII
+    
+    x = re.sub(r'(\r\n|\r|\n|\\n)+', ' ', x)  # normaliza quebras de linha
+    x = re.sub(r'[ \t]+', ' ', x)             # normaliza espaços em branco
+    x = x.replace("•", "-")                   # substitui marcador de lista
+    x = x.strip()                             # remove espaços em branco nas extremidades
+    return x
 
 
-# --------------------------------------------------
-# Carregar modelo traçado
-# --------------------------------------------------
+# --------------------------------------------------------------------------------------------
+# Carrega o modelo
+# --------------------------------------------------------------------------------------------
+
 device0 = "cuda" if torch.cuda.is_available() else "cpu"
 model = torch.jit.load("Python/model_traced.pt", map_location=device0)
 _ = model.eval()
 
-# --------------------------------------------------
-# Função de avaliação
-# --------------------------------------------------
+
+# --------------------------------------------------------------------------------------------
+# Função principal
+# --------------------------------------------------------------------------------------------
+
 def avaliar_prontuario(pront_teste, delta=10):
 
     perguntas = [
@@ -59,11 +70,13 @@ def avaliar_prontuario(pront_teste, delta=10):
     map_pred = {0: "Sim", 1: "Não", 2: "Sem informação"}
 
     # Prepara entrada
-    X = Encoder(normalizar_prontuario(pront_teste))
-    X = torch.tensor(fix_len(X, 8192), dtype=torch.long).unsqueeze(0).to(device0)
-
+    X = Encoder(normalizar_prontuario(pront_teste, unicode=True))
+    X = torch.tensor(fix_len(X, 2**13), dtype=torch.long)
+    X = X.unsqueeze(0).to(device0)
+    model.eval()
+    
     with torch.no_grad():
-        logits, scores, _ = model(X)              # modelo retorna (logits, scores)
+        logits, scores, wei = model(X)
         probs = F.softmax(logits, dim=1)
         preds = torch.argmax(logits, dim=1)
         preds = preds.squeeze(0)
@@ -76,14 +89,13 @@ def avaliar_prontuario(pront_teste, delta=10):
 
         # encontra token mais relevante para essa pergunta
         j = torch.argmax(scores[0, :, i]).item()
-
-        # extrai janela de texto em torno do token j
-        start = max(0, 2*(2*(2*j+1)+1)+1 - delta)
-        end   = min(len(pront_teste), 2*(2*(2*j+7)+7)+5 + delta)
+        R = 41   # receptive field
+        J = 8    # stride acumulado
+        start = max(0, j * J - delta)
+        end   = min(2**13, j * J + R + delta)
         frase = normalizar_prontuario(pront_teste, unicode=False)[start:end]
-        frase = frase.replace("\n", " ")       # remove quebras de linha
-        frase = " ".join(frase.split())        # normaliza espaços em branco
-
+        frase = frase.strip()
+        
         linhas.append([pergunta, map_pred[pred_idx], prob_pred, frase])
 
     # formata como tabela bonita
@@ -94,12 +106,13 @@ def avaliar_prontuario(pront_teste, delta=10):
     for p, pred, prob, frase in linhas:
         print(f"{p.ljust(col1_w)}  |  {pred.ljust(col2_w)}  |  {prob:.3f} |  {frase}")
 
-    # também retorna em DataFrame se quiser manipular
-    return pd.DataFrame(linhas, columns=["pergunta", "pred", "prob", "evidencia"])
+    return pd.DataFrame(linhas, columns=["pergunta", "pred", "prob", "evidencia"]), scores, wei
 
-# --------------------------------------------------
+
+# --------------------------------------------------------------------------------------------
 # Exemplo de uso
-# --------------------------------------------------
+# --------------------------------------------------------------------------------------------
+
 #with open("Dados/Teste1.txt", encoding="utf-8") as f:
 #    pront = f.read()
 #    df = avaliar_prontuario(pront)
